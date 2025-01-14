d6a60ef85b4a91abcd5bf8c3a01edd46f4e8635e
Vishwas4025 <vishwasracharla09@gmail.com>
2025-01-14 23:03:53 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+// require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+const cors = require('cors')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect("mongodb://127.0.0.1:27017/")
+  .then(() => {
+    // listen for requests
+    const PORT = 4000
+    app.listen(PORT, () => {
+      console.log('connected to db & listening on port', PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
